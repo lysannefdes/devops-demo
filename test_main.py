d2fa808743c8cc482a1ddf2943bceff5715fab5d
Lysanne Fernandes <lysanneferns@gmail.com>
2023-07-26 13:17:20 +0530

    update test file

--- test_main.py
+++ test_main.py
@@ -21,3 +21,6 @@ class TestCalculatorFunctions(unittest.TestCase):
     def test_divide_by_zero(self):
         with self.assertRaises(ValueError):
             divide(10, 0)
+
+if __name__ == "__main__":
+    unittest.main()
