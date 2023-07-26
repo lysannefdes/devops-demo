3a5a09414f99da8b9fe1c0a91372fbce25cf55a3
Lysanne Fernandes <lysanneferns@gmail.com>
2023-07-26 13:14:20 +0530

    Adding commit on feature branch

--- test_main.py
+++ test_main.py
@@ -1,7 +1,7 @@
 import unittest
-from main import add, subtract
+from calculator import add, subtract, multiply, divide
 
-class TestMathFunctions(unittest.TestCase):
+class TestCalculatorFunctions(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(2, 3), 5)
         self.assertEqual(add(-1, 1), 0)
@@ -10,5 +10,14 @@ class TestMathFunctions(unittest.TestCase):
         self.assertEqual(subtract(5, 2), 3)
         self.assertEqual(subtract(10, 7), 3)
 
-if __name__ == "__main__":
-    unittest.main()
+    def test_multiply(self):
+        self.assertEqual(multiply(2, 3), 6)
+        self.assertEqual(multiply(-4, 5), -20)
+
+    def test_divide(self):
+        self.assertEqual(divide(10, 2), 5)
+        self.assertEqual(divide(100, 25), 4)
+
+    def test_divide_by_zero(self):
+        with self.assertRaises(ValueError):
+            divide(10, 0)
